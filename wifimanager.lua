d77d3fa0343af3bb99db2fbeb7358b71ad0e734f
B. Nelissen <b.g.l.nelissen@gmail.com>
2018-12-25 22:29:20 +0100

    edits

--- wifimanager.lua
+++ wifimanager.lua
@@ -3,3 +3,21 @@
 
 -- print filename
 print("wifimanager.lua")
+
+-- check if file credentials.lua exists on device
+if file.open("credentials.lua") then
+  print(file.read())
+  file.close()
+  dofile("credentials.lua")
+  -- file exists, running on station mode
+  dofile("station_mode.lua")
+-- elseif file.open("ap_mode.lua") then
+--   -- file don't exists, runnning on ap mode
+--   print("Credentials file not found, going into AP mode")
+--   dofile("ap_mode.lua")
+else
+  print("Error, files not found:")
+  print("- credentials.lua")
+  print("- ap_mode.lua")
+end
+
