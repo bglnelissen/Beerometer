8838d077b2b7012ba08630a9f16f2a1ee931b8ef
Bastiaan Nelissen <bglnelissen@users.noreply.github.com>
2018-12-30 16:33:37 +0100

    added function structure to init file

--- init.lua
+++ init.lua
@@ -1,8 +1,22 @@
 --init.lua
+-- b.nelissen
+-- winter 2018/2019
+
+-- This file makes sure it all boots and is verbose on its actions for debugging.
+
+-- functions
+dofile('functions.lua')
 
 -- reset button
-dofile('reset.lua')
+if file_exists('reset.lua') then
+	dofile('reset.lua')
+else
+	print('Error, file does not exists:\n- reset.lua')
+end
 
 -- wifi
-dofile('wifimanager.lua')
-
+if file_exists('wifimanager.lua') then
+	dofile('wifimanager.lua')
+else
+	print('Error, file does not exists:\n- wifimanager.lua')
+end
